0ae09ad04cf9ab576fb3483cca27ec4c51163429
jarias <jacin@elrocin.es>
2016-10-15 13:48:34 +0200

    More gulp support

---
+++ _app/gulp/paths.js
@@ -0,0 +1,50 @@
+var paths = {};
+
+// Directory locations
+paths.appDir             = '_app/';  // The files Gulp will work on
+paths.jekyllDir          = '';       // The files Jekyll will work on
+paths.siteDir            = '_site/'; // The resulting static site
+
+
+// Folder naming conventions
+paths.postFolderName    = '_posts';
+paths.draftFolderName   = '_drafts';
+paths.pagesFolderName   = '_pages';
+paths.assetsFolderName  = 'assets';
+paths.imageFolderName   = 'images';
+
+
+// App files locations
+paths.appSassFiles   = paths.appDir  + "sass";
+paths.appImageFiles  = paths.appDir  + paths.imageFolderName;
+paths.appAssetsFiles = paths.appDir  + paths.assetsFolderName;
+
+
+// Jekyll files locations
+paths.jekyllPostFiles  = paths.jekyllDir + paths.postFolderName;
+paths.jekyllDraftFiles = paths.jekyllDir + paths.draftFolderName;
+paths.jekyllHtmlFiles  = paths.jekyllDir + paths.pagesFolderName;
+paths.jekyllImageFiles = paths.jekyllDir + paths.imageFolderName;
+
+paths.jekyllStyleFiles = paths.jekyllDir + paths.assetsFolderName;
+
+
+// Glob patterns by file type
+paths.sassPattern        = '/**/*.scss';
+paths.jsPattern          = '/**/*.js';
+paths.imagePattern       = '/**/*.+(jpg|JPG|jpeg|JPEG|png|PNG|svg|SVG|gif|GIF|webp|WEBP|tif|TIF)';
+paths.markdownPattern    = '/**/*.+(md|MD|markdown|MARKDOWN)';
+paths.htmlPattern        = '/**/*.html';
+paths.xmlPattern         = '/**/*.xml';
+
+// App files globs
+paths.appSassFilesGlob     = paths.appSassFiles     + paths.sassPattern;
+paths.appImageFilesGlob    = paths.appImageFiles    + paths.imagePattern;
+
+// Jekyll files globs
+paths.jekyllPostFilesGlob    = paths.jekyllPostFiles  + paths.markdownPattern;
+paths.jekyllDraftFilesGlob   = paths.jekyllDraftFiles + paths.markdownPattern;
+paths.jekyllHtmlFilesGlob    = paths.jekyllHtmlFiles  + paths.htmlPattern;
+paths.jekyllImageFilesGlob   = paths.jekyllImageFiles + paths.imagePattern;
+
+module.exports = paths;
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-const gulp = require('gulp');
+const gulp    = require('gulp');
+const concat  = require('gulp-concat');
+const sass    = require('gulp-sass');
 
-gulp.task('watch', () => {
+var paths = require('./_app/gulp/paths');
 
+gulp.task('css', () => {
+  gulp.src(paths.appSassFilesGlob)
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(paths.jekyllStyleFiles))
 });
 
-gulp.task('default', ['watch']);
+gulp.task('watch', () => {
+  gulp.watch(paths.appSassFilesGlob, ['css']);
+});
+
+gulp.task('default', ['css', 'watch']);
